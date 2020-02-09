c6662146d48d9f99f522c7de77141fd9fded76dc
Kimbeomchul <54543148+Kimbeomchul@users.noreply.github.com>
2020-02-09 19:41:07 +0900

    Image
    Image Get

--- flutter_instagram_clone/lib/search_page.dart
+++ flutter_instagram_clone/lib/search_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'create_page.dart';
 class SearchPage extends StatefulWidget {
   @override
   _SearchPageState createState() => _SearchPageState();
@@ -9,9 +9,35 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: null,
+      body: _buildBody(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => CreatePage()));
+        },
         child: Icon (Icons.create),
+        backgroundColor: Colors.blue,
+
       ),
     );
   }
 }
+
+ Widget _buildBody() {
+   return GridView.builder(
+       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+         crossAxisCount: 3,
+         childAspectRatio: 1.0,
+         mainAxisSpacing: 1.0,
+         crossAxisSpacing: 1.0, ),
+       itemCount: 5,
+       itemBuilder: (context, index) {
+     return _buildListItem(context, index);
+   });
+ }
+  Widget _buildListItem(BuildContext context, int index){
+      return Image.network('https://cdn.pixabay.com/photo/2017/09/21/19/12/france-2773030_1280.jpg', fit: BoxFit.cover);
+
+
+
+
+  }
